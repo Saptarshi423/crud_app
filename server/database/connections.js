bf7aa5db195807cd3d306869dc208b5c1827ab8e
Saptarshi423 <51373298+Saptarshi423@users.noreply.github.com>
2021-09-16 10:13:05 +0530

    Initial commit

---
+++ server/database/connections.js
@@ -0,0 +1,21 @@
+//using mongoose we can connect the mongodb database to our application.
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try{
+        // mongodb connection string
+        const con = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            
+        })
+
+        console.log(`MongoDB connected : ${con.connection.host}`);
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB
